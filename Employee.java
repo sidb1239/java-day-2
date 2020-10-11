282a8a2572f23d299331999f3c81542b4c132dd2
siddhartha singh <66827554+sidb1239@users.noreply.github.com>
2020-10-12 00:25:18 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,11 @@
+package lu;
+
+public class Employee {
+	String name ;
+	int age ;
+	String city;
+	
+	
+	
+
+}
---
+++ Main.java
@@ -0,0 +1,23 @@
+package lu;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee em = new Employee();
+		em.name ="siddhartha";
+		em.age =19;
+		em.city ="bangalore";
+		
+		System.out.println("the name is "+em.name);
+		System.out.println("the age is "+em.age);
+		System.out.println("the city is "+em.city);
+
+		
+		
+		
+
+	}
+
+}
